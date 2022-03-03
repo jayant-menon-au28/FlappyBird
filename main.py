53761c86e26916fb6a32295cf0591b7f449dcafa
JMenon <adm7utd@gmail.com>
2022-03-03 20:00:25 +0530

    added 2 types of keypress detection to main event loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants.constants import Colors
 
 pygame.init()
 screen = pygame.display.set_mode((Constants.HEIGHT, Constants.HEIGHT))
+background = Colors.GRAY
 
 running = True
 while running:
@@ -11,7 +12,22 @@ while running:
         # print(event) 
         if event.type == pygame.QUIT:
             running = False
-        screen.fill(Colors.GRAY)
+
+        screen.fill(background)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                background = Colors.RED
+            if event.key == pygame.K_LEFT:
+                background = Colors.GREEN
+
+
+
+        # keys_pressed = pygame.key.get_pressed()
+        # if keys_pressed[pygame.K_RIGHT]:
+        #     background = Colors.RED
+        
+        
         pygame.display.update()
 
 
